0f9246534de5bb54f390e6364ec1744e64be8eb5
test <sanviiz06@hotmail.com>
2022-03-11 00:45:34 +0700

    🚀 Deploy [11/03/22]

---
+++ _next/static/K4C7CrvcdCdLoQsOuwAAb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/94-a528e50d7ffbf12f.js","static/chunks/pages/index-8039ed2b9cb2fbe5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
