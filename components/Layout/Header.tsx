17bfe90010ffa186a278ff621c5fb74ea4f42a61
test <sanviiz06@hotmail.com>
2022-03-11 00:57:11 +0700

    Fix: some styling and seo optimization

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -94,9 +94,8 @@ const Header: FC = () => {
 								<div
 									className="block cursor-pointer border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 transition-all hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-gray-900 md:dark:hover:bg-transparent md:dark:hover:text-white"
 									onClick={() =>
-										window.open(
-											'https://github.com/sanviiz/sanviiz.github.io/tree/develop/'
-										)
+										(window.location.href =
+											'https://github.com/sanviiz/sanviiz.github.io/tree/develop/')
 									}
 								>
 									Source
